71ba90c2a4270efc91f3b535d7d1ac08181eeebd
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-11-13 20:23:40 -0800

    debugging

--- app/dashboard.component.js
+++ app/dashboard.component.js
@@ -2,13 +2,14 @@
 ;
 var DashboardComponent = (function () {
     function DashboardComponent(heroService) {
-        var _this = this;
         this.heroService = heroService;
         this.heroes = [];
-        this.ngOnInit = {
-            this: .heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes.slice(1, 5); })
-        };
     }
+    DashboardComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.heroService.getHeroes()
+            .then(function (heroes) { return _this.heroes = heroes.slice(1, 5); });
+    };
     return DashboardComponent;
 }());
 exports.DashboardComponent = DashboardComponent;
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -6,10 +6,15 @@ import { HeroService } from './hero.service';
 	selector: 'my-dashboard',
 	templateUrl: 'dashboard.component.html'
 });
-export class DashboardComponent implements OnInit { 
-	heroes: Hero[] = [];
-	constructor ( private heroService: HeroService){ }
-	ngOnInit: void{
-		this.heroService.getHeroes().then( heroes=> this.heroes = heroes.slice(1,5));
-	}
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
 }
+
